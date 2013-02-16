e33d5ee4aa6c5019dd1215c2173e305786e1f457
quinton <qlpetty@gmail.com>
2013-02-16 13:54:35 -0800

    No Black Screen

--- Totem/SleepViewController.m
+++ Totem/SleepViewController.m
@@ -27,7 +27,7 @@
 {
     [super viewDidLoad];
     // Enabled monitoring of the sensor
-    [[UIDevice currentDevice] setProximityMonitoringEnabled:YES];
+    //[[UIDevice currentDevice] setProximityMonitoringEnabled:YES];
     
     // Set up an observer for proximity changes
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sensorStateChange:)
